1172dca02b8b21a1f5ca13b8bad719573aa8f043
abhishek9125 <aagarwal_be17@thapar.edu>
2022-07-28 02:08:46 +0530

    Added: Config Environment

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import config from 'config/env';
 
 export default () => (
   <div>
-    <Title>My First Next.js Page</Title>
+    <Title>My First Next.js Page {config.domainName}</Title>
   </div>
 );
 
