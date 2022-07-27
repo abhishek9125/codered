133a42318d61e7e4d427f1bd7784cb52eb710bd3
abhishek9125 <aagarwal_be17@thapar.edu>
2022-07-28 03:05:22 +0530

    Added: Stylesheets

--- frontend/components/PageLayout/index.js
+++ frontend/components/PageLayout/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import { ThemeProvider } from "styled-components";
+import GlobalStyle from 'theme/GlobalStyles';
 import theme from '../../theme';
 
 function PageLayout({ children }) {
@@ -11,6 +12,7 @@ function PageLayout({ children }) {
                 <Header />
                 {children}
                 <Footer />
+                <GlobalStyle />
             </Fragment>
         </ThemeProvider>
     )
