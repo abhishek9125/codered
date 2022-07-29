5c1b5b77310e91a4d5dfc6b617ad3d6416e6a52f
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2022-07-29 14:22:43 +0530

    Initialized: Backend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { readdirSync } = require('fs');
+require('dotenv').config();
+
+// App Initialisation
+const app = express();
+
+// Database Setup
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.log("Database Error : ", error));
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '4mb' }));
+app.use(cors());
+
+// Routes
+readdirSync('./routes').map((route) => app.use('/api', require(`./routes/${route}`)));
+
+// Port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is Running on Port ${port}`);
+})
