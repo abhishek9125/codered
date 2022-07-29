05575d586ce1b328b09b7f96c0a219855010cb54
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2022-07-29 23:26:48 +0530

    Added: Authentication Routes

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user');
+
+exports.authCheck = async (req, res, next) => {
+    try {
+
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+
+        }
+
+    } catch (error) {
+        console.log("Error Validating the Token : ", error);
+        res.status(401).json({
+            error: 'Token is either Invalid or Expired.'
+        })
+    }
+}
